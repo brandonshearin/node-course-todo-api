91c166232b43b61f3028acb4ce1bb3d3fb89b6d9
brandonshearin <bshearin15@gmail.com>
2017-08-06 10:46:35 -0400

    added PATCH /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -117,6 +117,46 @@ describe('GET /todos/:id', () => {
 });
 
 
+describe('DELETE /todos/:id', () => {
+	it('should remove a todo', (done) => {
+		var hexId = todos[1]._id.toHexString();
+
+		request(app)
+		.delete(`/todos/${hexId}`)
+		.expect(200)
+		.expect((res) => {
+			expect(res.body._id).toBe(hexId);
+		})
+		.end((err, res) => {
+			if(err){
+				return done(err);
+			}
+			//query database using findById
+			Todo.findById(hexId).then((todo) => {
+				expect(todo).toNotExist();
+				done();
+			}).catch((err) => {done(err)});
+		});
+	});
+
+	it('should return 404 if todo not found', (done) => {
+		var idee = new ObjectID();
+		request(app)
+		.delete(`/todos/${idee}.toHexString()`)
+		.expect(404)
+		.end(done);
+	});
+
+	it('should return 404 if object id is invalid', (done) => {
+		var idee2 = 123;
+		request(app)
+		.delete(`/todos/${idee2}`)
+		.expect(404)	
+		.end(done);
+	});
+
+});
+
 
 
 
