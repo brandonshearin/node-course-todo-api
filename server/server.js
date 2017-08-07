b1a0ee3f9338b213ecd6b5b1dc018487a6628a51
brandonshearin <bshearin15@gmail.com>
2017-08-07 15:10:05 -0400

    add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate.js');
 
 var app = express();
 //if PORT is defined  express uses that variable, if not then localhost 3000
@@ -106,6 +107,9 @@ app.post('/users', (req, res) => {
 });
 
 
+app.get('/users/me', authenticate,(req, res) => {
+	res.send(req.user);
+});
 
 app.listen(port, () => {
 	console.log(`started up at port ${port}`);
