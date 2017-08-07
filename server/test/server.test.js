a17906bcfec903ed2cad6c0a9ec27a11eb42ebc2
brandonshearin <bshearin15@gmail.com>
2017-08-07 17:11:01 -0400

    Add DELETE /users/me/token route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -244,6 +244,57 @@ describe('POST /users', () =>{
 	});
 });
 
+// describe('POST /users/login', () => {
+//   it('should login user and return auth token', (done) => {
+//     request(app)
+//       .post('/users/login')
+//       .send({
+//         email: users[1].email,
+//         password: users[1].password
+//       })
+//       .expect(200)
+//       .expect((res) => {
+//         expect(res.headers['x-auth']).toExist();
+//       })
+//       .end((err, res) => {
+//         if(err){
+//           return done;
+//         }
+//         User.findById(users[1]._id).then((user) => {
+//           console.log(users[1]);
+//           expect(user.tokens[0]).toInclude({
+//             access: 'auth',
+//             token: res.headers['x-auth']
+//           });
+//           done();
+//         }).catch((err) => done(err));
+//       });
+//   });
+
+//   it('should reject invalid login', (done) => {
+//     request(app)
+//       .post('/users/login')
+//       .send({
+//         email: users[1].email,
+//         password: users[1].password +'1'
+//       })
+//       .expect(400)
+//       .expect((res) => {
+//         expect(res.headers['x-auth']).toNotExist();
+//       })
+//       .end((err, res) => {
+//         if(err){
+//           return done;
+//         }
+//         User.findById(users[1]._id).then((user) => {
+//           console.log(users[1]);
+//           expect(user.tokens[0]).toBe(0);
+//           done();
+//         }).catch((err) => done(err));
+//       });
+//   });
+
+// });
 
 
 
